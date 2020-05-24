4e10d6e4b1d50ae9bcbe648abe42873e99a548f0
Mateus Gonçalo <mateus.goncalo@ice.ufjf.br>
2020-05-24 10:47:42 -0300

    Ajustando server.js

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-//Importa as dependências que acabamos de instalar
 const express = require('express');
 const path = require('path');
-
+const nomeApp = process.env.npm_package_name;
 const app = express();
-
-// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/dist/black-dashboard-angular'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/black-dashboard-angular/index.html'));
+ 
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+ 
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
-
-// Inicia a aplicação pela porta configurada
+ 
 app.listen(process.env.PORT || 8080);
