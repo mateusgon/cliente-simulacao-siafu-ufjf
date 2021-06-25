20bd8b6a0d9f837a68a223c9539b0be5e2a16098
Mateus Gonçalo <mateus.goncalo@ice.ufjf.br>
2021-06-25 09:11:03 -0300

    Adding about text

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -7,7 +7,6 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: "about.component.html"
 })
 export class AboutComponent implements OnInit {
-  public description: string;
 
   constructor(private simulationService: SimulationService, config: NgbCarouselConfig) {
     config.interval = 10000;  
@@ -17,6 +16,5 @@ export class AboutComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.description = "O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500, quando uma misturou os caracteres de um texto para criar um espécime de livro. Este texto não só sobreviveu 5 séculos, mas também o salto para a tipografia electrónica, mantendo-se essencialmente inalterada. Foi popularizada nos anos 60 com a disponibilização das folhas de Letraset, que continham passagens com Lorem Ipsum, e mais recentemente com os programas de publicação como o Aldus PageMaker que incluem versões do Lorem Ipsum.";
   }
 }
